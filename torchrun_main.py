c2ef2112ec983479a90d5ce3dce1ab5e1ff48e81
Gabriel Puliatti <gabriel@emptor.io>
2023-07-14 00:54:46 -0500

    -

--- torchrun_main.py
+++ torchrun_main.py
@@ -296,7 +296,7 @@ def main(args):
 
     # it doesn't matter which tokenizer we use, because we train from scratch
     # T5 tokenizer was trained on C4 and we are also training on C4, so it's a good choice
-    tokenizer = LlamaTokenizer.from_pretrained(model_path)
+    tokenizer = LlamaTokenizer.from_pretrained(args.model_config)
 
     def preprocess_batched(batch):
         batch = tokenizer(
